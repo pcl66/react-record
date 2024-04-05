9ca00106da9de23fba8e6401eab8df8b6687813a
1500371773@qq.com <1500371773@qq.com>
2024-04-06 05:19:11 +0800

    修改skipWelcome为key值

--- src/pages/welcome/components/Welcome4.tsx
+++ src/pages/welcome/components/Welcome4.tsx
@@ -5,7 +5,7 @@ import { WelcomeTemplate } from './WelcomeTemplate'
 export const Welcome4 = () => {
   const nav = useNavigate()
   const next = () => {
-    localStorage.setItem('welcome', 'true')
+    localStorage.setItem('skipWelcome', 'true')
     nav('/home')
   }
   const hSwipeChange = (direction: string) => {
