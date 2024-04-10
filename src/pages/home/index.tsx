a532065efa14e31781ea60402e67e70f6ad2342b
1500371773@qq.com <1500371773@qq.com>
2024-04-11 01:04:04 +0800

    创建mock

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from 'react'
+import axios from 'axios'
 import zz from '../../assets/山竹.svg'
 import { Button } from '../../components/button'
 
 export const Home = () => {
+  useEffect(() => {
+    // todo
+    axios.get('http://localhost:5173/api/v1/me')
+  })
   return (
     <div className='px-2'>
       <div className='flex justify-center items-center mt-[20vh] mb-[10vh]'>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteMockServe } from 'vite-plugin-mock'
 import { svgsprites } from './src/vite_plugin/svgsprites'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgsprites()],
+  plugins: [
+    react(),
+    viteMockServe(),
+    svgsprites(),
+  ],
 })
